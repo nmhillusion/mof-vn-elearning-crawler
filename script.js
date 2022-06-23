80745e167152f069e9664961e43332396cf5dfc9
nmhillusion <nmhillusion@users.noreply.github.com>
2022-06-23 14:57:47 +0000

    deploy: c8a39bf0ecd5932a58b483fbbaf770e76d2a00ad

---
+++ script.js
@@ -0,0 +1,60 @@
+"use strict";
+(function main() {
+    console.log("MOF eLearning Crawler");
+    const PATTERN__QUESTION = /onmousemove="Tip\(&#39;(.+?)&#39;\)"/;
+    const CORRECT_MARK = "[*]";
+    const btnSubmit = document.querySelector("#btn-submit");
+    const inpContentEl = document.querySelector("#input-box");
+    const resultListEl = document.querySelector("#result-list");
+    btnSubmit.onclick = (e) => {
+        const inpContent = inpContentEl.value.trim();
+        if (!inpContent) {
+            alert("Content must be not empty");
+            return;
+        }
+        console.log("input content: ", { inpContent });
+        const questionsMatching = inpContent.matchAll(new RegExp(PATTERN__QUESTION, "gi"));
+        console.log({ questionsMatching });
+        if (questionsMatching) {
+            while (resultListEl.firstChild) {
+                resultListEl.removeChild(resultListEl.firstChild);
+            }
+            let questionIdx = 0;
+            for (const qsMatching of questionsMatching) {
+                const [_matched, qsGroup] = qsMatching;
+                const parts = String(qsGroup)
+                    .split(/&lt;br \/\>/)
+                    .map((it) => it.replace(/&lt;\/?(br|b)(.*?)>/g, ""))
+                    .map((it) => it.replace(/&lt;font(.*?)>/g, ""))
+                    .map((it) => it.replace(/&lt;\/font(.*?)>/g, CORRECT_MARK))
+                    .filter((e) => e);
+                console.log({ parts });
+                buildQuestionFromParts(questionIdx, parts);
+                questionIdx += 1;
+            }
+            if (0 == questionIdx) {
+                alert("Does not contains any question from source-code");
+            }
+        }
+    };
+    function buildQuestionFromParts(questionIdx, parts = []) {
+        if (parts && 0 < parts.length) {
+            parts.unshift(`[${questionIdx + 1}]`);
+            parts.push("---");
+            for (const part of parts) {
+                const trEl = document.createElement("tr");
+                const tdEl = document.createElement("td");
+                tdEl.textContent = part;
+                if (part.endsWith(CORRECT_MARK)) {
+                    tdEl.classList.add("correct");
+                }
+                trEl.appendChild(tdEl);
+                resultListEl.appendChild(trEl);
+            }
+        }
+        else {
+            alert("Does not contains any question from source-code");
+        }
+    }
+})();
+//# sourceMappingURL=script.js.map
